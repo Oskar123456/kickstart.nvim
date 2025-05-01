71c230e6ec63b939b194b9e69e1270dddf6038c2
oskar123456 <cph-oh82@cphbusiness.dk>
2025-05-01 17:14:04 +0200

    border

--- init.lua
+++ init.lua
@@ -9,6 +9,8 @@ vim.opt.guicursor = 'n-v-i-c:block-Cursor'
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
+vim.o.winborder = 'single'
+
 -- [[ Setting options ]]
 -- See `:help vim.opt`
 -- NOTE: You can change these options as you wish!
